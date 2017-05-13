82096971d97d1960e628d64b4e2efffee5ed92dc
IhsanSayim <ihsansaym@gmail.com>
2017-05-13 22:25:50 +0300

    Add All Files

---
+++ MvcBlog/Controllers/AdminUyeController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcBlog.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcBlog.Controllers
+{
+    public class AdminUyeController : Controller
+    {
+        private mvcblogDB db = new mvcblogDB();
+
+        // GET: AdminUye
+        public ActionResult Index(int Page = 1)
+        {
+            var uyes = db.Uyes.Include(u => u.Yetki);
+            return View(uyes.OrderByDescending(u=>u.UyeId).ToPagedList(Page,10));
+        }
+
+        // GET: AdminUye/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // GET: AdminUye/Create
+        public ActionResult Create()
+        {
+            ViewBag.YetkiId = new SelectList(db.Yetkis, "YetkiId", "Yetki1");
+            return View();
+        }
+
+        // POST: AdminUye/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UyeId,KullaniciAdi,Email,Sifre,AdSoyad,YetkiId")] Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Uyes.Add(uye);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.YetkiId = new SelectList(db.Yetkis, "YetkiId", "Yetki1", uye.YetkiId);
+            return View(uye);
+        }
+
+        // GET: AdminUye/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.YetkiId = new SelectList(db.Yetkis, "YetkiId", "Yetki1", uye.YetkiId);
+            return View(uye);
+        }
+
+        // POST: AdminUye/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UyeId,KullaniciAdi,Email,Sifre,AdSoyad,YetkiId")] Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uye).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.YetkiId = new SelectList(db.Yetkis, "YetkiId", "Yetki1", uye.YetkiId);
+            return View(uye);
+        }
+
+        // GET: AdminUye/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: AdminUye/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var uyes = db.Uyes.Where(u => u.UyeId == id).SingleOrDefault();
+                if (uyes == null)
+                {
+                    return HttpNotFound();
+                }
+               
+                foreach (var i in uyes.Yorums.ToList())
+                {
+                    db.Yorums.Remove(i);
+                }
+                foreach (var i in uyes.Makales.ToList())
+                {
+                    var makales = db.Makales.Where(m => m.UyeId == id).SingleOrDefault();
+                    if (makales == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    if (System.IO.File.Exists(Server.MapPath(makales.Foto)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(makales.Foto));
+                    }
+                   
+                    foreach (var ii in makales.Etikets.ToList())
+                    {
+                        db.Etikets.Remove(ii);
+                    }
+                    db.Makales.Remove(i);
+                }
+                db.Uyes.Remove(uyes);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
